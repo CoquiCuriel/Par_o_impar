f619dad112c235734c62186937699eb095a52ec9
Ricardo Curiel <66967495+CoquiCuriel@users.noreply.github.com>
2022-05-05 23:52:34 -0300

    Add files via upload
    Se pide un numero al usuario y se indica si es par o impar.

---
+++ par_o_impar.py
@@ -0,0 +1,14 @@
+"""
+
+Ricardo Curiel. 
+
+Pedir un numero y verificar si es par o impar.
+
+"""
+
+numero = int(input("Hola, ingrese un número por favor."))
+
+if(numero % 2 == 0):
+    print("El número es par")
+else:
+    print("El número es impar")
