fcf9597b84ab72f14c032168795d1f19fcbde3fe
Ricardo Curiel <66967495+CoquiCuriel@users.noreply.github.com>
2022-05-06 00:06:33 -0300

    Add files via upload

--- par_o_impar.py
+++ par_o_impar.py
@@ -6,9 +6,16 @@ Pedir un numero y verificar si es par o impar.
 
 """
 
-numero = int(input("Hola, ingrese un número por favor."))
+salir = 'a'
 
-if(numero % 2 == 0):
-    print("El número es par")
-else:
-    print("El número es impar")
+while(salir != 's'):
+
+    numero = int(input("Hola, ingrese un número por favor."))
+
+    if(numero % 2 == 0):
+        print("El número es par")
+    else:
+        print("El número es impar")
+        
+    salir = input("para salir presione s(salir), sino Enter")
+
